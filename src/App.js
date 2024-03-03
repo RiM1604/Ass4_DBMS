1f47e197cc9d0c219a3e69e120a07e7b49bab319
Ritesah Madhunala <irnmadhunala123@gmail.com>
2024-03-03 19:00:01 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+// import Navbar from './components/Navbar';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import StudentSignUp from './components/StudentSignUp';
+import OrganizerSignUp from './components/OrganizerSignUp';
+import AdminSignUp from './components/AdminSignUp';
+import OtherSignUp from './components/OtherSignUp';
+import Events from './components/Events';
+import StudentLogin from './components/StudentLogin';
+import OrganizerLogin from './components/OrganizerLogin';
+import AdminLogin from './components/AdminLogin';
+import OtherLogin from './components/OtherLogin';
+import StudentPage from './components/StudentPage';
+import AdminPage from './components/AdminPage';
+import OrganizerPage from './components/OrganizerPage';
+import OtherPage from './components/OtherPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/student_signup" element={<StudentSignUp />}></Route>
+          <Route path="/organizer_signup" element={<OrganizerSignUp />}></Route>
+          <Route path="/admin_signup" element={<AdminSignUp />}></Route>
+          <Route path="/other_signup" element={<OtherSignUp />}></Route>
+          <Route path="/student_login" element={<StudentLogin />}></Route>
+          <Route path="/organizer_login" element={<OrganizerLogin />}></Route>
+          <Route path="/admin_login" element={<AdminLogin />}></Route>
+          <Route path="/other_login" element={<OtherLogin />}></Route>
+          <Route path="/events" element={<Events />}></Route>
+          <Route path="/student_page" element={<StudentPage />}></Route>
+          <Route path="/admin_page" element={<AdminPage />}></Route>
+          <Route path="/organizer_page" element={<OrganizerPage />}></Route>
+          <Route path="/other_page" element={<OtherPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
